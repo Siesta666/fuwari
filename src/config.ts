2773a74f2d6d120a295eedaea8d1d9c5bd11ee81
Siesta <1169391936@qq.com>
2026-07-21 20:39:31 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Siesta Repository",
-	subtitle: "Demo Site",
+	title: "Siesta Blog",
+	subtitle: "Everything Deserves A Try",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
